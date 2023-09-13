703b09bfb1adfc30496c438c5eb72576c296e00c
Abdirahman-oz <abdichamps@Xubuntu.myguest.virtualbox.org>
2023-09-13 02:09:37 -0400

    edited server.js

--- a1/server.js
+++ a1/server.js
@@ -1,1 +1,14 @@
-console.log('HELLO-WORLD my nan')
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Abdirahman Osman Student ID: 115185175 Date: ________________
+*
+*  Online (Cyclic) URL: _______________________________________________________
+*
+********************************************************************************/ 
+console.log('Hello')
+
+
